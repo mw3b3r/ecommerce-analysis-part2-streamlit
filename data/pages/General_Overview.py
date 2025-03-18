040f32020ec72da66527ebf4227d5dd6aff2bc36
mw3b3r <mart.wbr@gmail.com>
2025-03-18 20:33:03 +0000

    committed

--- data/pages/General_Overview.py
+++ data/pages/General_Overview.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.title("General Overview")
 st.write("Obsah pro stránku General Overview")
+
+st.metric(label="Total Unique Transactions", value=df["TransactionNo"].nunique())
